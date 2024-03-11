0071644ca2b503e1a998c3e6b2a37bfa80c9c8cf
Tao Starbow <tstarbow@salesforce.com>
2024-03-11 14:44:27 -0700

    Basic config file parsing in place

--- README.txt
+++ README.txt
@@ -1,19 +1,22 @@
-Setup tasks:
-# only needed for javascript?
-# 1. eslint 
-# - Add .eslintrc.js
-#     - setup jest
-# - npm install eslint 
-#     - configure > npm init @eslint/config
-#     - Needs work :(
-2. Typescript
-- npm install --save-dev typescript
+Tool for watching a folder and automatically running handbrake on new files.
+Also supports limited file renaming.
+
+Current Development Setup:
+1. Install node for your environment
+2. Download code
+3. Setup projects
+   > npm install
+4. Install Handbrake-CLI
+
+Initial Development Setup tasks:
+1. Typescript
+  > npm install --save-dev typescript
     - configure: > tsc --init
-- node-ts?
-3. Jest (w/Typescript)
-- npm install --save-dev jest
-- npm install --save-dev @types/jest
-- npm install --save-dev ts-jest
+  > npm install --save-dev ts-node
+2. Jest (w/Typescript)
+  > npm install --save-dev jest
+  > npm install --save-dev @types/jest
+  > npm install --save-dev ts-jest
     - configure (https://kulshekhar.github.io/ts-jest/docs/getting-started/installation/#jest-config-file)
-    - npx ts-jest config:init
-- vs code config: Add to setting.json -  "jest.pathToConfig" : "./jest.config.js" 
+    > npx ts-jest config:init
+  > VScode config: Add to setting.json -  "jest.pathToConfig" : "./jest.config.js" 
--- index.ts
+++ index.ts
@@ -1,14 +1,56 @@
 import { watch, getQueueStats } from "./handbraker";
 
-import { program } from "commander";
-program.version("1.0.0", "-v, --version").usage("[OPTIONS]...");
+import * as toml from "toml";
+import * as fs from "fs";
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("Handbraker")
+  .description("Add a watch folder to handbrake")
+  .version("1.0.0", "-v, --version")
+  .usage("[OPTIONS]...");
+// .option(
+//   "-f, --config_file [path]",
+//   "Config File in TOML format",
+//   "handbraker.config.toml"
+// );
+
+// program.on("option:config_file", () => {
+//   console.log("Setup configs");
+// });
 
 program
   .command("watch")
   .description("Watch the input directory")
-  .action(() => {
+  .option(
+    "-f, --config_file [path]",
+    "Config File, in TOML format.",
+    "handbraker.config.toml"
+  )
+  .option("-i, --input_folder [path]", "Folder to watch for incoming files.")
+  .option("-o, --output_folder [path]", "Folder to write to.")
+  .option("-m, --max [int]", "Max number of jobs to run at once.", "1")
+  .option("-p, --handbrake [params]", "Parameters to pass to handbrake-cli.")
+  .action((opts) => {
     console.log("Watch command called");
-    watch();
+    console.log("Looking for config file: ", opts.config_file);
+    let fileConfigs: Record<string, any> = {};
+    if (opts.config_file) {
+      fileConfigs = toml.parse(fs.readFileSync(opts.config_file, "utf-8"));
+      console.log("fileConfigs:", fileConfigs);
+    }
+    const configs: Record<string, string> = {};
+    configs["input_folder"] =
+      opts.input_folder ?? fileConfigs.input_folder ?? "watch";
+    configs["output_folder"] =
+      opts.output_folder ?? fileConfigs.output_folder ?? "output";
+    configs["max_jobs"] = opts.max_jobs ?? fileConfigs.max_jobs ?? "1"; // TODO -- convert to int.
+    configs["handbrake"] =
+      opts.handbrake ?? fileConfigs.handbrake ?? "Fast 480p30";
+    console.log("configs:", configs);
+    //    watch(configs);
   });
 
 program
@@ -19,4 +61,5 @@ program
   });
 
 program.parse(process.argv);
-const options = program.opts();
+// const options = program.opts();
+// console.log(options);
